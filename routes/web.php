807c863ffe760e0e984a012f89809d96469c4c22
Mito Chikhladze <chikhladze.mt@gmail.com>
2020-07-02 16:34:15 +0400

    123

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::group(['prefix' => LaravelLocalization::setLocale(), 'middleware' => [ 'localeSessionRedirect', 'localizationRedirect', 'localeViewPath']], function(){
-	//MAIN CONTROLLER
-	Route::group(['prefix'=>'/'], function() {
-		
-	});
+Route::get('/clear-cache', function() {
+    Artisan::call('cache:clear');
+    return "Cache is cleared";
 });
