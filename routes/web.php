17845f7912483b98a8fa78d8c7393a132d3968a2
Mito Chikhladze <chikhladze.mt@gmail.com>
2020-07-02 16:14:53 +0400

    123123123

--- routes/web.php
+++ routes/web.php
@@ -13,3 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::group(['prefix' => LaravelLocalization::setLocale(), 'middleware' => [ 'localeSessionRedirect', 'localizationRedirect', 'localeViewPath']], function(){
+
+});
