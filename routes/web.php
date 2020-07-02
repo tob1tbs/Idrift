60c44a913c61d32094194dd4d5d43b73d3da2cc2
Mito Chikhladze <chikhladze.mt@gmail.com>
2020-07-02 16:08:46 +0400

    1

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::group(['prefix' => LaravelLocalization::setLocale(), 'middleware' => [ 'localeSessionRedirect', 'localizationRedirect', 'localeViewPath']], function(){
+Route::group(['prefix' => LaravelLocalization::setLocale()], function() {
 
 });
